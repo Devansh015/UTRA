d07ce62df5749b15cfb4bd74c3839523728658e8
SaqAsh <s5ashraf@uwaterloo.ca>
2026-01-31 17:25:59 -0500

    both move

--- UTRA/src/main.cpp
+++ UTRA/src/main.cpp
@@ -1,9 +1,9 @@
-
 #include <Arduino.h>
 
-// New Mapping based on your move
+// Motor A (Left)
 const int IN1 = 9;  
 const int IN2 = 10; 
+// Motor B (Right)
 const int IN3 = 6;  
 const int IN4 = 5;  
 
@@ -12,26 +12,34 @@ void setup() {
   pinMode(IN2, OUTPUT);
   pinMode(IN3, OUTPUT);
   pinMode(IN4, OUTPUT);
+  
   Serial.begin(9600);
+  Serial.println("UTRA: Syncing Motors..."); 
+}
+
+void forwardFullPower() {
+  // Motor A - High
+  digitalWrite(IN1, HIGH);
+  digitalWrite(IN2, LOW);
+
+  // Motor B - High
+  digitalWrite(IN3, HIGH);
+  digitalWrite(IN4, LOW);
 }
 
 void stopAll() {
-  digitalWrite(IN1, LOW); digitalWrite(IN2, LOW);
-  digitalWrite(IN3, LOW); digitalWrite(IN4, LOW);
+  digitalWrite(IN1, LOW);
+  digitalWrite(IN2, LOW);
+  digitalWrite(IN3, LOW);
+  digitalWrite(IN4, LOW);
 }
 
 void loop() {
-  // TEST 1: ONLY LEFT (Motor A)
-  Serial.println("Testing LEFT only...");
-  digitalWrite(IN1, HIGH); 
-  delay(2000);
+  Serial.println("Full speed ahead!");
+  forwardFullPower();
+  delay(3000); // Run for 3 seconds
+  
+  Serial.println("Halt.");
   stopAll();
-  delay(1000);
-
-  // TEST 2: ONLY RIGHT (Motor B)
-  Serial.println("Testing RIGHT only...");
-  digitalWrite(IN3, HIGH); 
-  delay(2000);
-  stopAll();
-  delay(1000);
+  delay(2000); // Wait for 2 seconds
 }
